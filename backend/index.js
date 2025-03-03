324aa426f1ff3df9d8c7131f8bd29732037e8dae
kemila <kemiladabare4@gmail.com>
2025-03-03 19:33:09 +0530

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import router from "./src/routes/routes.js";
 import { dbConnect } from "./src/config/dbConfig.js";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use(cors, (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 const port = 3000;
 
 dbConnect();
