d190bf843b0b4b3c0a39a4b68f35903e4ccb7d39
kemila <kemiladabare4@gmail.com>
2025-03-04 10:44:01 +0530

    added build folder

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,21 @@ import express from "express";
 import router from "./src/routes/routes.js";
 import { dbConnect } from "./src/config/dbConfig.js";
 import cors from "cors";
+import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "/public")));
+
+const port = process.env.PORT || 5000;
 
 dbConnect();
 
